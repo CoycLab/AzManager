9465ecf62259db0f25bf0c3f8c9ad2878c907557
Aliaksei Vasilkou <aliaksei.vasilkou@gmail.com>
2019-02-17 21:54:31 +0300

    Spring framework initialization

---
+++ src/main/java/com/github/coyclab/azmanager/controller/ProductController.java
@@ -0,0 +1,10 @@
+package com.github.coyclab.azmanager.controller;
+
+import com.github.coyclab.azmanager.model.Product;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+}
